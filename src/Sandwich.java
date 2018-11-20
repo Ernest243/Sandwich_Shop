62751d8a891555ad352ad5676093a3e7a264d87f
Ernest B.M 243 <emudjambere@gmail.com>
2018-11-20 07:57:17 -0800

    Create Sandwich.java
    This is the launcher of the program. This creates a new object of the "OrderCalculatorGUI" class which which also creates object of every other classes.

---
+++ src/Sandwich.java
@@ -0,0 +1,13 @@
+/*
+   This class creates 
+   a new object of the 
+   OrderCalculatorGUI class, 
+   and lunch it.
+*/
+public class Sandwich
+{
+   public static void main(String[] args)
+   {
+     new OrderCalculatorGUI(); // Creating new object
+   }
+}
